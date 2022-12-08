b31663fe53bb729e4775639f39cfbdc3a7d1a53e
Preston <prestonip8@gmail.com>
2022-12-07 21:20:13 -0500

    Add formatted grammar and parse tables screenshots

---
+++ grammar.py
@@ -0,0 +1,45 @@
+'''
+S' -> YOINKY <stmt> SPLOINKY
+<stmt> -> <var_stmt>
+<stmt> -> <select_stmt>
+<stmt> -> <loop_stmt>
+<stmt> -> <block>
+<var_stmt> -> id
+<var_stmt> -> id = <expr>
+<select_stmt> ->  maybe ( <bool_expr> ) <stmt> 
+<select_stmt> ->  maybe ( <bool_expr> ) <stmt> actually <block>
+<loop_stmt> ->  loop ( <bool_expr> ) <block> 
+<block> -> { <stmt> ; }
+<block> -> { }
+<expr> -> <term> 
+<expr> -> <term> * <term>
+<expr> -> <term> - <term>
+<term> -> <factor> 
+<term> -> <factor> + <factor>
+<term> -> <factor> / <factor>
+<term> -> <factor> % <factor>
+<factor> -> id
+<factor> -> int_lit
+<factor> -> ( <expr> )
+
+<bool_inc> -> <bool_eval>
+<bool_inc> -> <bool_eval> and <bool_eval>
+<bool_inc> -> <bool_eval> or <bool_eval>
+<bool_eval> -> <bool_expr>
+<bool_eval> -> <bool_expr> != <bool_expr>
+<bool_eval> -> <bool_expr> == <bool_expr>
+<bool_eval> -> <bool_expr> >= <bool_expr>
+<bool_eval> -> <bool_expr> <= <bool_expr>
+<bool_eval> -> <bool_expr> < <bool_expr>
+<bool_eval> -> <bool_expr> > <bool_expr>
+<bool_expr> -> <bool_term> 
+<bool_expr> -> <bool_term> * <bool_term>
+<bool_expr> -> <bool_term> - <bool_term>
+<bool_term> -> <bool_factor>
+<bool_term> -> <bool_factor> + <bool_factor>
+<bool_term> -> <bool_factor> / <bool_factor>
+<bool_term> -> <bool_factor> % <bool_factor>
+<bool_factor> -> id 
+<bool_factor> -> int_lit 
+<bool_factor> -> bool_lit
+'''
